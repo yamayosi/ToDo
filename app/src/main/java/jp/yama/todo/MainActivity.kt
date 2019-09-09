3e8d9ab1f888d470d1a6d19b47fde82b371f16ba
yamayoshi <vanha.muist@gmail.com>
2019-09-09 22:43:43 +0900

    first commit

---
+++ app/src/main/java/jp/yama/todo/MainActivity.kt
@@ -0,0 +1,29 @@
+package jp.yama.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import jp.yama.todo.Fragment.Outer
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //  fragment exec
+        val fragmentManager = supportFragmentManager
+        displayFragment(fragmentManager)
+
+    }
+
+    companion object{
+        fun displayFragment(fragmentManager: FragmentManager){
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            val fragment = Outer.newInstance("init", "fragment")
+            fragmentTransaction.replace(R.id.fragment_outer, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+    }
+}
